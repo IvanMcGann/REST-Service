dac830333fd857d5f48df7873b0bd49a220a5892
Ivan McGann <ivanmcganngti@gmail.com>
2019-11-28 19:09:30 +0000

    Updated User class to return specified values.

--- REST-Service/src/main/java/ie/gmit/ds/User.java
+++ REST-Service/src/main/java/ie/gmit/ds/User.java
@@ -8,11 +8,23 @@ public class User {
 	private String userName;
 	private String email;
 	private String hashedPassword;
-	private String salt;
+    private String salt;
+    
+    private String password;
 
 	public User() {
 		// Needed for Jackson deserialisation
-	}
+    }
+    // Used for create and update operations.
+
+    public User(int userId, String userName, String email, String password){
+        this.userId = userId;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+    }
+
+    // Used to return info on specific users and the list of all users.
 
 	public User(int userId, String userName, String email, String hashedPassword, String salt) {
 		this.userId = userId;
@@ -45,6 +57,11 @@ public class User {
 	@JsonProperty
 	public String getSalt() {
 		return salt;
+    }
+    
+    @JsonProperty
+	public String getPassword() {
+		return password;
 	}
 
 }
